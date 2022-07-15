bc3ebccdea275d1138e8ee15426b84034872ba29
Stephin Jose <sjstephinkj@gmail.com>
2022-07-15 13:09:33 -0400

    Add Auth0 integration for SSO

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AuthModule } from '@auth0/auth0-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthButtonComponent } from './components/auth-button/auth-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-7b0bndfn.us.auth0.com',
+      clientId: 'Q0i1UUinMoWJkLH4g6JbUXkVs4G9Ay0I'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
