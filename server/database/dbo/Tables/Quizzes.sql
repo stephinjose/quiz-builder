277172f24aad8144420fb4d548426a0eae204cf4
Stephin Jose <sjstephinkj@gmail.com>
2022-07-18 13:46:55 -0400

    Add Quizzes table

---
+++ server/database/dbo/Tables/Quizzes.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Quizzes] (
+    [Id]        INT            IDENTITY (1, 1) NOT NULL,
+    [UserId]    INT            NOT NULL,
+    [Title]     NVARCHAR (500) NOT NULL,
+    [CreatedOn] DATETIME       CONSTRAINT [DF_Quizzes_CreatedOn] DEFAULT (getutcdate()) NOT NULL,
+    [Permalink] NCHAR (6)      NOT NULL,
+    CONSTRAINT [PK_Quizzes] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Quizzes_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id])
+);
+
