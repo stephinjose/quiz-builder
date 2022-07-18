74a62a7fb6de4e376b56bc247770d1e3278b261f
Stephin Jose <sjstephinkj@gmail.com>
2022-07-18 14:58:26 -0400

    Make question type unique

--- server/database/dbo/Tables/QuestionTypes.sql
+++ server/database/dbo/Tables/QuestionTypes.sql
@@ -1,7 +1,10 @@
 ﻿CREATE TABLE [dbo].[QuestionTypes] (
     [Symbol] CHAR (1)   NOT NULL,
-    [Type]   NCHAR (10) NOT NULL
+    [Type]   NCHAR (10) NOT NULL,
+    CONSTRAINT [UQ_QuestionType] UNIQUE CLUSTERED ([Symbol] ASC)
 );
+
+
 
 
 
