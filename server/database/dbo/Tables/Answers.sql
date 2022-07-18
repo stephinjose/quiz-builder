6039aa381b633b0f112d1d15d97e0df91eae523d
Stephin Jose <sjstephinkj@gmail.com>
2022-07-18 16:42:51 -0400

    Add Answers table

---
+++ server/database/dbo/Tables/Answers.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Answers] (
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [QuestionId] INT            NOT NULL,
+    [SortOrder]  INT            NOT NULL,
+    [Text]       NVARCHAR (MAX) NOT NULL,
+    [IsCorrect]  BIT            NOT NULL,
+    CONSTRAINT [PK_Answers] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Answers_Questions] FOREIGN KEY ([QuestionId]) REFERENCES [dbo].[Questions] ([Id])
+);
+
