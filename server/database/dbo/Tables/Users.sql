e18476baabc4355eccf7e0981f7711cd7f6c1012
Stephin Jose <sjstephinkj@gmail.com>
2022-07-18 13:40:26 -0400

    Add Users table

---
+++ server/database/dbo/Tables/Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]        INT            IDENTITY (1, 1) NOT NULL,
+    [Auth0Id]   NVARCHAR (200) NOT NULL,
+    [Email]     NVARCHAR (200) NOT NULL,
+    [CreatedOn] DATETIME       CONSTRAINT [DF_Users_CreatedOn] DEFAULT (getutcdate()) NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
