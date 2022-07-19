841f2f4d08c1aad9bc3c4d2b05ca8cd5e1a07571
Stephin Jose <sjstephinkj@gmail.com>
2022-07-19 10:46:04 -0400

    Initial styling

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,11 +2,12 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AuthHttpInterceptor, AuthModule } from '@auth0/auth0-angular';
-
+import { MatButtonModule } from '@angular/material/button'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthButtonComponent } from './components/auth-button/auth-button.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,11 +19,12 @@ import { UserProfileComponent } from './components/user-profile/user-profile.com
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    MatButtonModule,
     AuthModule.forRoot({
       domain: 'dev-7b0bndfn.us.auth0.com',
       clientId: 'Q0i1UUinMoWJkLH4g6JbUXkVs4G9Ay0I',
       audience: "http://localhost:4200/api",
-      scope:'email',
+      scope: 'email',
       httpInterceptor: {
         allowedList: [
           {
@@ -32,7 +34,8 @@ import { UserProfileComponent } from './components/user-profile/user-profile.com
           }
         ]
       }
-    })
+    }),
+    BrowserAnimationsModule
   ],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true },],
   bootstrap: [AppComponent]
