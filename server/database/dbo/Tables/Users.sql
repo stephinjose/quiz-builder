38c22447a53bce36e62ee8da27b13ccab88df419
Stephin Jose <sjstephinkj@gmail.com>
2022-07-19 09:58:37 -0400

    Add index on Auth0Id and Email

--- server/database/dbo/Tables/Users.sql
+++ server/database/dbo/Tables/Users.sql
@@ -3,6 +3,14 @@
     [Auth0Id]   NVARCHAR (200) NOT NULL,
     [Email]     NVARCHAR (200) NOT NULL,
     [CreatedOn] DATETIME       CONSTRAINT [DF_Users_CreatedOn] DEFAULT (getutcdate()) NOT NULL,
-    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [UQ_Users_Email] UNIQUE NONCLUSTERED ([Email] ASC)
 );
+
+
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_Users_Auth0Id]
+    ON [dbo].[Users]([Auth0Id] ASC);
 
